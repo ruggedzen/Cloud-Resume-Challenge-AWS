17624cc51d097ed7752468a86e86a2da3cc63d6a
Alex <ahiggins@ruggedzen.com>
2024-05-16 14:38:16 -0700

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -71,13 +71,13 @@ resource "aws_s3_bucket_policy" "allow_read_all" {
 
 
 #R53 Zone
-resource "aws_route53_zone" "primary" {
+resource "aws_route53_zone" "swnl_zone" {
   name = "sheepwithnolegs.com"
 }
 
-#TODO: Get Digital cert from ACM for sheepwithnolegs.com
+#Get cert from ACM for sheepwithnolegs.com
 resource "aws_acm_certificate" "swnl_cert" {
-  domain_name = "sheepwithnolegs.com"
+  domain_name       = "sheepwithnolegs.com"
   validation_method = "DNS"
 
   lifecycle {
@@ -86,15 +86,35 @@ resource "aws_acm_certificate" "swnl_cert" {
 
   tags = {
     Project = "CRC"
-  } 
-}
-#TODO: DNS Cert auth
-resource "aws_route53_record" "swnl_cert_auth" {
-  name = aws_acm_certificate.swnl_cert.validation_method
-  
+  }
 }
 
+#Create CNAME to auth ACM provided cert
+resource "aws_route53_record" "swnl_cert_cname" {
+  for_each = {
+    for dvo in aws_acm_certificate.swnl_cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = aws_route53_zone.swnl_zone.zone_id
+}
+
+#Validate ACM Cert
+resource "aws_acm_certificate_validation" "swnl_cert_val" {
+  certificate_arn         = aws_acm_certificate.swnl_cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.swnl_cert_cname : record.fqdn]
+}
+
+
+#TODO: CloudFront Distro
 #TODO: A record for CloudFront distro
 #TODO: Change NS in GoDaddy with R53 NS
-#TODO: CloudFront Distro
 
