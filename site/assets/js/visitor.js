7eda4093e6f12e1fe3ec9617e8ccb2b0a5aef89e
Alex <ahiggins@ruggedzen.com>
2024-06-18 16:24:59 -0700

    Fix invoke url

--- site/assets/js/visitor.js
+++ site/assets/js/visitor.js
@@ -1,4 +1,4 @@
-fetch("https://ekxt6jfn62.execute-api.us-east-1.amazonaws.com/test")//invoke url
+fetch("https://ekxt6jfn62.execute-api.us-east-1.amazonaws.com/test/visitor")
     .then(response => response.json())
     .then((data) => {
         document.getElementById('visitor').innerText = data.count
