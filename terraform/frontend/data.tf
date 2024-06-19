b2e113971039d9cd70f630d3cd63c238f7f5b6a2
Alex <ahiggins@ruggedzen.com>
2024-06-18 17:45:51 -0700

    rollback oidc changes

--- terraform/frontend/data.tf
+++ terraform/frontend/data.tf
@@ -13,29 +13,3 @@ data "aws_iam_policy_document" "allow_read_all" {
     ]
   }
 }
-
-data "aws_iam_policy_document" "oidc_github_policy"{
-  statement {
-    principals {
-      type = "AWS"
-      identifiers = aws_iam_openid_connect_provider.github_oidc.arn
-    }
-
-    "Version": "2012-10-17",
-    "Statement": [
-      {            
-        "Effect": "Allow",
-        "Principal": {
-          "Federated": "aws_iam_openid_connect_provider.github_oidc.arn"
-        },
-        "Action": "sts:AssumeRoleWithWebIdentity",
-        "Condition": {
-          "StringEquals": {
-            "token.actions.githubusercontent.com:aud": "sts.amazonaws.com",
-            "token.actions.githubusercontent.com:sub": "repo:ruggedzen/Cloud-Resume-Challenge-AWS:ref:refs/heads/main"
-          }
-        }
-      }
-    ] 
-  }
-}
