e66ed39576f43b5d46bf705707422657a0d51b0e
Alex <ahiggins@ruggedzen.com>
2024-06-20 16:27:08 -0700

    Update visitor.js

--- site/assets/js/visitor.js
+++ site/assets/js/visitor.js
@@ -1,6 +1,6 @@
 const counter = document.querySelector("#visitor");
 async function updateCounter() {
-    let response = await fetch(" https://ke1c0veccc.execute-api.us-east-1.amazonaws.com/add_visitor_dynamo_lambda");
+    let response = await fetch("https://aqgkrnjhth.execute-api.us-east-1.amazonaws.com/crc_lambda");
     let data = await response.json();
     counter.innerHTML = `${data}`;
 }
