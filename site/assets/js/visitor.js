4bb6305125d98d9f82ccd7fb7a1915728250b662
Alex <ahiggins@ruggedzen.com>
2024-06-20 12:39:20 -0700

    General fixes

--- site/assets/js/visitor.js
+++ site/assets/js/visitor.js
@@ -1,4 +1,4 @@
-const counter = document.querySelector("#visiter");
+const counter = document.querySelector("#visitor");
 async function updateCounter() {
     let response = await fetch(" https://ke1c0veccc.execute-api.us-east-1.amazonaws.com/add_visitor_dynamo_lambda");
     let data = await response.json();
