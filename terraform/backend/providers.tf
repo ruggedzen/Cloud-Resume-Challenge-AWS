79fc2a1b5a2cdbeab553a1be82ffa325aa9e66f0
Alex <ahiggins@ruggedzen.com>
2024-06-20 13:30:25 -0700

    Getting started

---
+++ terraform/backend/providers.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.49.0"
+    }
+  }
+  required_version = ">= 1.2.0"
+}
+#Provider Conifg
+provider "aws" {
+  region  = var.aws_region
+  profile = var.cli_profile
+}
