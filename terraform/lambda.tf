e25b10b407e578fb4354ce762ab93b9f9ca6124a
Alex <ahiggins@ruggedzen.com>
2024-06-20 19:05:58 -0700

    terraform lambda deployment

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -4,17 +4,19 @@ resource "aws_iam_role" "swnl_lambda_role" {
   managed_policy_arns = [aws_iam_policy.swnl_lambda_policy.arn]
 }
 
-data "archive_file" "swnl_lambda_code" {
+data "archive_file" "dummy_code" {
   type        = "zip"
-  source_file = "lambda_function.py"
   output_path = "lambda_function_payload.zip"
+  source {
+    content = "hello"
+    filename = "dummy.txt"
+  }
 }
 
 resource "aws_lambda_function" "swnl_lambda" {
-  filename         = "lambda_function_payload.zip"
+  filename         = "${data.archive_file.dummy_code.output_path}"
   function_name    = "crc_lambda"
   role             = aws_iam_role.swnl_lambda_role.arn
-  source_code_hash = data.archive_file.swnl_lambda_code.output_base64sha256
   runtime          = "python3.12"
   handler          = "lambda_function.lambda_handler"
 
